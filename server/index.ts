314e69e0d3d6b6a12e769cb9a5ca526dc8300745
se030 <se030.kim@gmail.com>
2022-11-15 04:51:49 +0900

    feat: 에러 핸들러 미들웨어 추가

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import env from "@config";
 import authRouter from "@apis/auth/controller";
 import workspaceRouter from "@apis/workspace/controller";
+import errorHandler from "@middlewares/error-handler";
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,7 @@ app.use(cookieParser(env.COOKIE_SECRET_KEY));
 app.get("/", (req: Request, res: Response) => res.send("Express"));
 app.use("/auth", authRouter);
 app.use("/workspace", workspaceRouter);
+
+app.use(errorHandler);
 
 app.listen(8080);
