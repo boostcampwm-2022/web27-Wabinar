5580179d7c5c3189acf8b3ef1eb7ca7ab6b060dd
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-14 17:52:54 +0900

    chore : 완성되지 않은 로그인 페이지 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>wab</div>;
 }
 
 export default App;
