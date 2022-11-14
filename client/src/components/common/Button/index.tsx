47684e8a6663f79a41de488ff3c77d535bedd3a7
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-14 21:56:20 +0900

    feat : 버튼 공통 컴포넌트 구현

--- client/src/components/common/Button/index.tsx
+++ client/src/components/common/Button/index.tsx
@@ -1,13 +1,15 @@
 import $ from "./style.module.scss";
+import cx from "classnames";
 
 interface ButtonProps {
   text: string;
-  icon: JSX.Element;
+  icon?: JSX.Element;
+  className?: string;
 }
 
-function Button({ text, icon }: ButtonProps) {
+function Button({ text, icon, className }: ButtonProps) {
   return (
-    <div className={$.button}>
+    <div className={cx($.button, className)}>
       {icon}
       <button>{text}</button>
     </div>
