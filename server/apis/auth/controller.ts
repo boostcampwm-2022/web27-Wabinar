c2dfdd7d8476eaddc399895a1403be852be32a36
se030 <se030.kim@gmail.com>
2022-11-15 00:49:33 +0900

    feat: auth 컨트롤러 레이어 로직 작성 - 로그인, 로그아웃
    Co-authored-by: Ruby <happyu9986@gmail.com>
    Co-authored-by: Won-hee Cho <wcho21@users.noreply.github.com>
    Co-authored-by: 백도훈 <dohun31@users.noreply.github.com>

---
+++ server/apis/auth/controller.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, NextFunction } from "express";
+import * as authService from "./service";
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { code } = req.body;
+
+      const loginToken = await authService.login(code);
+
+      const cookieOptions = {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 1000 * 60 * 60 * 24,
+      };
+      res.cookie("accessToken", loginToken, cookieOptions);
+
+      res.status(200).send();
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+router.get("/logout", (req: Request, res: Response) => {
+  res.clearCookie("accessToken");
+  res.status(200).send();
+});
+
+export default router;
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "@apis/auth/controller";
 
 const app = express();
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => res.send("Express"));
 app.use("/auth", authRouter);
