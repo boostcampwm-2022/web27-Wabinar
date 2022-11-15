a7df41a23091721891d07f3d3f70a2711de5aa4f
se030 <se030.kim@gmail.com>
2022-11-15 18:28:48 +0900

    feat: 로그인 페이지 버튼에 로그인을 위한 url 지정
    components/common/Button 컴포넌트 href props 여부에 따라 a tag 사용하도록 변경

--- client/src/components/common/Button/index.tsx
+++ client/src/components/common/Button/index.tsx
@@ -1,19 +1,25 @@
-import cx from "classnames";
+import cx from 'classnames';
 
-import style from "./style.module.scss";
+import style from './style.module.scss';
 
 interface ButtonProps {
   text: string;
+  href?: string;
   icon?: JSX.Element;
   className?: string;
 }
 
-function Button({ text, icon, className }: ButtonProps) {
-  return (
-    <div className={cx(style.button, className)}>
+function Button({ href, text, icon, className }: ButtonProps) {
+  return href ? (
+    <a href={href} className={cx(style.button, className)}>
       {icon}
-      <button>{text}</button>
-    </div>
+      <span>{text}</span>
+    </a>
+  ) : (
+    <button className={cx(style.button, className)}>
+      {icon}
+      <span>{text}</span>
+    </button>
   );
 }
 
