53efe2f70611f31e764e8e564759152038e77deb
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-15 17:14:50 +0900

    feat : isDisable, color 버튼 props 추가

--- client/src/components/common/Button/index.tsx
+++ client/src/components/common/Button/index.tsx
@@ -1,18 +1,31 @@
-import cx from "classnames";
+import classNames from 'classnames/bind';
 
-import style from "./style.module.scss";
+import style from './style.module.scss';
+
+const cx = classNames.bind(style);
 
 interface ButtonProps {
   text: string;
   icon?: JSX.Element;
   className?: string;
+  isDisable?: boolean;
+  color?: string;
 }
 
-function Button({ text, icon, className }: ButtonProps) {
+function Button({
+  text,
+  icon,
+  className,
+  isDisable = false,
+  color,
+}: ButtonProps) {
   return (
-    <div className={cx(style.button, className)}>
+    <div
+      className={cx(style.button, className, { disable: isDisable })}
+      style={{ backgroundColor: color }}
+    >
       {icon}
-      <button>{text}</button>
+      <button disabled={isDisable}>{text}</button>
     </div>
   );
 }
