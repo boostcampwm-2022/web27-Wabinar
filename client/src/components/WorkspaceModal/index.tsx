9a25b52398934878abfeaf0fbc9b929b6ec9eaf8
dohun <dohun31@naver.com>
2022-11-17 15:04:15 +0900

    feat: 상위 컴포넌트에 code state 선언
    CreateModal의 로직 결과인 code를 CreateSuccessModal에 공유해야해서 상위 컴포넌트에 state 생성
    
    Resolve: #21

--- client/src/components/WorkspaceModal/index.tsx
+++ client/src/components/WorkspaceModal/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { MENU, MODAL_MENUS } from 'src/constants/workspace';
 
 import CreateModal from './CreateModal';
@@ -13,6 +14,8 @@ function WorkspaceModal({
   selectedMenu,
   setSelectedMenu,
 }: WorkspaceModalProps) {
+  const [code, setCode] = useState<string>('');
+
   const modalContents = MODAL_MENUS[selectedMenu];
 
   const onClose = () => setSelectedMenu(0);
@@ -24,11 +27,16 @@ function WorkspaceModal({
           modalContents={modalContents}
           onClose={onClose}
           setSelectedMenu={setSelectedMenu}
+          setCode={setCode}
         />
       );
     case MENU.CREATE_SUCCESS:
       return (
-        <CreateSuccessModal modalContents={modalContents} onClose={onClose} />
+        <CreateSuccessModal
+          modalContents={modalContents}
+          onClose={onClose}
+          code={code}
+        />
       );
     case MENU.JOIN:
       return <JoinModal modalContents={modalContents} onClose={onClose} />;
