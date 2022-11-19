705f16dcac8974b868201ff422ba61ee0193f2ad
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-20 05:59:23 +0900

    refactor : 로그인 & auth 시 워크스페이스 리스트 가져오도록 변경

--- server/apis/auth/controller.ts
+++ server/apis/auth/controller.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express';
-import asyncWrapper from '@utils/async-wrapper';
+import { CREATED, OK } from '@constants/http-status';
 import jwtAuthenticator from '@middlewares/jwt-authenticator';
-import * as authService from './service';
-import { OK, CREATED } from '@constants/http-status';
 import { PostLoginParams } from '@params/auth';
+import asyncWrapper from '@utils/async-wrapper';
+import express, { Request, Response } from 'express';
+import * as userService from '../user/service';
+import * as authService from './service';
 
 interface CookieOptions {
   httpOnly: boolean;
@@ -18,16 +19,33 @@ router.get(
   '/',
   jwtAuthenticator,
   asyncWrapper(async (req: Request, res: Response) => {
-    res.status(OK).send(req.user);
+    if (!req.user) {
+      res.status(OK).send({ user: req.user });
+      return;
+    }
+
+    // req.user가 존재하면 workspaceList를 같이 받아옴
+    const { id: userId, name, avatarUrl } = req.user;
+
+    const workspaceList = await userService.getWorkspaces(userId);
+
+    const user = { id: userId, name, avatarUrl };
+
+    res.status(OK).send({ user, workspaceList });
   }),
 );
 
+// authorized된 유저가 아닐 경우(위에서 !req.user조건으로 return 됨)
+// OAuth 페이지에서 workspace로 이동(navigate)하게 되므로
+// 로그인 하여 받아온 유저 정보로 workspace list 정보도 함께 받아온다.
 router.post(
   '/login',
   asyncWrapper(async (req: Request<PostLoginParams>, res: Response) => {
     const { code } = req.body;
 
     const { user, loginToken, refreshToken } = await authService.login(code);
+
+    const workspaceList = await userService.getWorkspaces(Number(user.id));
 
     const cookieOptions: CookieOptions = {
       httpOnly: true,
@@ -38,7 +56,7 @@ router.post(
     res.cookie('accessToken', loginToken, cookieOptions);
     res.cookie('refreshToken', refreshToken, cookieOptions);
 
-    res.status(CREATED).send(user);
+    res.status(CREATED).send({ user, workspaceList });
   }),
 );
 
