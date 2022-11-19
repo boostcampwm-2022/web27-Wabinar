500603685901b90b78efe3f701a29e5dd8972f43
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-20 06:02:27 +0900

    chore : workspace의 id, name만 받아오도록 수정

--- server/apis/user/service.ts
+++ server/apis/user/service.ts
@@ -2,17 +2,20 @@ import userModel from '@apis/user/model';
 import workspaceModel from '@apis/workspace/model';
 import AuthorizationError from '@errors/authorization-error';
 
-export const getWorkspaces = async (targetUserId: number, userId: number) => {
+export const getWorkspaces = async (userId: number, targetUserId?: number) => {
   if (targetUserId !== userId)
     throw new AuthorizationError(
       '요청하신 유저 정보와 현재 로그인된 유저 정보가 달라요 ^^',
     );
 
-  const user = await userModel.findOne({ id: targetUserId });
+  const user = await userModel.findOne({ id: userId });
 
-  const workspaces = await workspaceModel.find({
-    id: { $in: user.workspaces },
-  });
+  const workspaces = await workspaceModel.find(
+    {
+      id: { $in: user.workspaces },
+    },
+    { id: 1, name: 1 },
+  );
 
   return workspaces;
 };
