a4d76f8310f6909bd9671fd4505554373e89e88b
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-23 13:04:34 +0900

    chore : 필요없는 socket room join은 제거

--- server/socket/signaling.ts
+++ server/socket/signaling.ts
@@ -4,16 +4,6 @@ function signalingSocketServer(io: Server) {
   const signaling = io.of(/^\/signaling\/\d+$/);
 
   signaling.on('connection', (socket) => {
-    const name = socket.nsp.name;
-    const workspaceId = name.match(/\d+/g)[0];
-
-    if (!workspaceId) {
-      socket.disconnect();
-      return;
-    }
-
-    socket.join(workspaceId);
-
     socket.on('offer', (sdp, roomId) => {
       socket.to(roomId).emit('offer', sdp);
     });
