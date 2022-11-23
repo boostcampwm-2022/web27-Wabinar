312d8d43ea71d8f73f8149976c0e35e98e0591a7
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-23 23:33:52 +0900

    feat : WebRTC 시그널링 구현 (테스트전)
    socket 이벤트 연동 - join, offer, answer, ice-candidate
    setMyStream: 내 비디오 설정 함수 분리
    setPeerConnection: 각각의 피어 연결을 위한 세팅 함수 분리
    IParticipant: 참여자들 타입 정의 -> 아직 뭐가 더 필요한지 모르겠음..

--- client/src/pages/Workspace/index.tsx
+++ client/src/pages/Workspace/index.tsx
@@ -1,14 +1,25 @@
 import Workspace from 'components/Workspace';
 import WorkspaceList from 'components/WorkspaceList';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Socket } from 'socket.io-client';
 import { getWorkspaceInfo } from 'src/apis/workspace';
+import ConfBar from 'src/components/ConfBar';
+import { RTC } from 'src/constants/socket-message';
+import useSocket from 'src/hooks/useSocket';
+import { IParticipant } from 'src/types/rtc';
 import { WorkspaceInfo } from 'src/types/workspace';
 
 import style from './style.module.scss';
 
+interface IPeerConnection {
+  [id: string]: RTCPeerConnection;
+}
+
 function WorkspacePage() {
   const { id } = useParams();
+  const socket = useSocket(`/signaling/${id}`);
+
   const [workspace, setWorkspace] = useState<WorkspaceInfo | null>(null);
 
   const loadWorkspaceInfo = async () => {
@@ -22,16 +33,124 @@ function WorkspacePage() {
     loadWorkspaceInfo();
   }, []);
 
+  const peerConnectionConfig = {
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+    ],
+  };
+
+  const myStreamRef = useRef<MediaStream | null>(null);
+  const myVideoRef = useRef<HTMLVideoElement | null>(null);
+  const [participants, setParticipants] = useState<IParticipant[]>([]);
+  const peerConnectionRef = useRef<IPeerConnection | null>(null);
+
+  const setMyStream = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    myStreamRef.current = stream;
+
+    if (myVideoRef.current) {
+      myVideoRef.current.srcObject = myStreamRef.current;
+    }
+  };
+
+  /* Peer와 연결하기 */
+  const setPeerConnection = (socket: Socket, socketId: string) => {
+    const peerConnection = new RTCPeerConnection(peerConnectionConfig);
+
+    myStreamRef.current?.getTracks().forEach((track) => {
+      if (!myStreamRef.current) return;
+
+      peerConnection.addTrack(track, myStreamRef.current); // 다른 유저에게 전달해주기 위해 내 미디어를 peerConnection 에 추가한다.
+    });
+
+    /* 이벤트 핸들러: Peer에게 candidate를 전달 할 필요가 있을때 마다 발생 */
+    peerConnection.onicecandidate = (e) => {
+      const candidate = e.candidate;
+      if (candidate) {
+        socket.emit(RTC['ice-candidate'], candidate);
+      }
+    };
+
+    /* 이벤트 핸들러: peerConnection에 새로운 트랙이 추가됐을 경우 호출됨
+      -> 누군가 내 offer를 remoteDescription에 설정했을 때? 
+    */
+    peerConnection.ontrack = (e) => {
+      // 새로운 peer를 참여자에 추가
+      setParticipants((participants) => [
+        ...participants,
+        { socketId, stream: e.streams[0] },
+      ]);
+    };
+
+    return peerConnection;
+  };
+
+  useEffect(() => {
+    setMyStream(); // 내 비디오 스트림 설정
+
+    /* 유저 join */
+    socket.emit(RTC.join);
+
+    /* 다른 유저 join */
+    socket.on(RTC.join, (participants) => {
+      participants.forEach(async (participant: IParticipant) => {
+        const { socketId } = participant;
+        const peerConnection = setPeerConnection(socket, socketId);
+
+        peerConnectionRef.current = {
+          ...peerConnectionRef.current,
+          [socketId]: peerConnection,
+        };
+
+        const offer = await peerConnection.createOffer();
+        peerConnection.setLocalDescription(offer);
+
+        socket.emit(RTC.offer, { socketId: participant.socketId, offer });
+      });
+    });
+
+    /* offer 받기 */
+    socket.on(RTC.offer, async ({ socketId, offer }) => {
+      const peerConnection = setPeerConnection(socket, socketId);
+      await peerConnection.setRemoteDescription(offer);
+
+      const answer = await peerConnection.createAnswer();
+      await peerConnection.setLocalDescription(answer);
+
+      /* answer 전송 */
+      socket.emit(RTC.answer, { answer });
+    });
+
+    /* answer 받기 */
+    socket.on(RTC.answer, async ({ socketId, answer }) => {
+      const peerConnection = peerConnectionRef?.current?.[socketId];
+      if (!peerConnection) return;
+      peerConnection.setRemoteDescription(answer);
+    });
+
+    /* ice candidate */
+    socket.on(RTC['ice-candidate'], async ({ socketId, answer }) => {
+      const peerConnection = peerConnectionRef?.current?.[socketId];
+      if (!peerConnection) return;
+      peerConnection.setRemoteDescription(answer);
+    });
+  }, []);
+
   return (
     <div className={style.container}>
       <WorkspaceList />
-      {workspace && ( // TODO: 임시로 만들었어요
+      {workspace && (
         <Workspace
           name={workspace.name}
           members={workspace.members}
           moms={workspace.moms}
         />
       )}
+      <ConfBar participants={participants} />
     </div>
   );
 }
