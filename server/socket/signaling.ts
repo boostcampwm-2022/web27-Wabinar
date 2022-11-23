6ec16a879b7334ed31c89fe611edfc30d2b98312
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-23 23:35:06 +0900

    feat : users 배열에 socketId 별로 구분하여 참여 유저 관리

--- server/socket/signaling.ts
+++ server/socket/signaling.ts
@@ -2,8 +2,20 @@ import { Server } from 'socket.io';
 
 function signalingSocketServer(io: Server) {
   const signaling = io.of(/^\/signaling\/\d+$/);
+  const users: string[] = [];
 
   signaling.on('connection', (socket) => {
+    socket.on('join', () => {
+      const socketId = socket.id;
+      console.log(socketId, '들어옴');
+
+      users.push(socketId);
+
+      const otherUser = users.filter((id) => id !== socketId);
+
+      socket.emit('join', otherUser);
+    });
+
     socket.on('offer', (sdp, roomId) => {
       socket.to(roomId).emit('offer', sdp);
     });
