a825bbab1649354354c2a5d0138c57577d081ffc
se030 <se030.kim@gmail.com>
2022-11-26 22:15:15 +0900

    refactor: RTC 로직 ConfMediaBar로 이동

--- client/src/pages/Workspace/index.tsx
+++ client/src/pages/Workspace/index.tsx
@@ -2,23 +2,15 @@ import Workspace from 'components/Workspace';
 import WorkspaceList from 'components/WorkspaceList';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { io } from 'socket.io-client';
 import { getWorkspaceInfo } from 'src/apis/workspace';
 import ConfMediaBar from 'src/components/ConfMediaBar';
-import config from 'src/config';
-import { useConfMediaStreams } from 'src/hooks/useConfMediaStreams';
 import { WorkspaceInfo } from 'src/types/workspace';
 
 import style from './style.module.scss';
 
 function WorkspacePage() {
-  const [workspace, setWorkspace] = useState<WorkspaceInfo | null>(null);
-
   const { id } = useParams();
-  const socketUrl = `${config.SERVER_PATH}/signaling/${id}`;
-  const socket = io(socketUrl);
-
-  const confMediaStreams = useConfMediaStreams(socket);
+  const [workspace, setWorkspace] = useState<WorkspaceInfo | null>(null);
 
   const loadWorkspaceInfo = async () => {
     if (id) {
@@ -41,7 +33,7 @@ function WorkspacePage() {
           moms={workspace.moms}
         />
       )}
-      <ConfMediaBar streams={confMediaStreams} />
+      <ConfMediaBar workspaceId={id} />
     </div>
   );
 }
