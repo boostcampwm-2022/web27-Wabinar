d2e4aa07ac4c5e19e98b463d8202b77f95e1c5d1
dohun <dohun31@naver.com>
2022-11-29 11:15:43 +0900

    refactor: 함수 분리
    toggle하는 함수가 따로 없고 map 안에서 처리하고 있었는데 나중에 api를 붙일 경우를 생각해서 따로 함수로 분리
    
    Resolve: #140

--- client/src/components/QuestionBlock/index.tsx
+++ client/src/components/QuestionBlock/index.tsx
@@ -29,15 +29,33 @@ const initialQuestion: Question[] = [
 function QuestionBlock() {
   const [questions, setQuestions] = useState<Question[]>(initialQuestion);
 
-  const onClick = (e: React.MouseEvent<HTMLLIElement>) => {
-    const { questionId: targetId } = e.currentTarget.dataset;
+  const toggleQuestion = (question: Question): Question => {
+    /** TODO api 붙이기 */
+    question.isDone = !question.isDone;
+    return question;
+  };
 
-    const nextQuestions = questions.map((question) => {
-      if (question.id === Number(targetId)) {
-        question.isDone = !question.isDone;
-      }
-      return question;
-    });
+  const createQuestion = (question: string) => {
+    /** TODO api 붙이기 */
+    const lastId = questions.at(-1)?.id;
+
+    const nextId = lastId !== undefined ? lastId + 1 : 0;
+
+    const newQuestion = {
+      id: nextId,
+      isDone: false,
+      question,
+    };
+
+    setQuestions([...questions, newQuestion]);
+  };
+
+  const onClick: React.MouseEventHandler<HTMLLIElement> = (e) => {
+    const { id: targetId } = e.currentTarget;
+
+    const nextQuestions = questions.map((question) =>
+      question.id === Number(targetId) ? toggleQuestion(question) : question,
+    );
 
     setQuestions(nextQuestions);
   };
@@ -56,20 +74,6 @@ function QuestionBlock() {
     target.value = '';
   };
 
-  const createQuestion = (question: string) => {
-    const lastId = questions.at(-1)?.id;
-
-    const nextId = lastId !== undefined ? lastId + 1 : 0;
-
-    const newQuestion = {
-      id: nextId,
-      isDone: false,
-      question,
-    };
-
-    setQuestions([...questions, newQuestion]);
-  };
-
   return (
     <div className={style['question-container']}>
       <h3 className={style.title}>질문 리스트</h3>
@@ -79,7 +83,7 @@ function QuestionBlock() {
             className={style['question-item']}
             onClick={onClick}
             key={id}
-            data-question-id={id}
+            id={id.toString()}
           >
             {isDone ? (
               <BiCheckboxChecked className={cx('check-box', { check: true })} />
