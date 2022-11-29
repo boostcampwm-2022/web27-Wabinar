1ac0a2dae14bd0c71357b194bdbbdd7797956faa
dohun <dohun31@naver.com>
2022-11-29 17:00:17 +0900

    feat: Button 컴포넌트 재사용
    원래 그냥 <button />을 사용했는데 commons/Button을 사용
    
    Resolve: #147

--- client/src/components/VoteBlock/index.tsx
+++ client/src/components/VoteBlock/index.tsx
@@ -2,6 +2,7 @@ import { BiX } from '@react-icons/all-files/bi/BiX';
 import classNames from 'classnames/bind';
 import React, { useState, useRef } from 'react';
 
+import Button from '../common/Button';
 import style from './style.module.scss';
 
 const cx = classNames.bind(style);
@@ -82,12 +83,12 @@ function VoteBlock() {
             ></input>
           </div>
           <div className={style['vote-buttons']}>
-            <button className={style.button} onClick={onAdd}>
-              항목 추가
-            </button>
-            <button className={style.button} onClick={onRegister}>
-              투표 등록
-            </button>
+            <Button className={style.button} onClick={onAdd} text="항목 추가" />
+            <Button
+              className={style.button}
+              onClick={onRegister}
+              text="투표 등록"
+            />
           </div>
         </>
       )}
