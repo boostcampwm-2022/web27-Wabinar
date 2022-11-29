1727629d813eb85630d8ba1472c77353f0a4d3bb
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-29 14:41:47 +0900

    feat : Mic, Cam 버튼을 합친 하나의 StreamButton 제작

---
+++ client/src/components/ConfMediaBar/StreamButton/index.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import CamButton from './CamButton';
+import MicButton from './MicButton';
+import style from './style.module.scss';
+
+interface StreamButtonProps {
+  isMicOn: boolean;
+  isCamOn: boolean;
+  setIsMicOn?: Dispatch<SetStateAction<boolean>>;
+  setIsCamOn?: Dispatch<SetStateAction<boolean>>;
+}
+
+function StreamButton({
+  isMicOn,
+  isCamOn,
+  setIsMicOn,
+  setIsCamOn,
+}: StreamButtonProps) {
+  return (
+    <div className={style['stream-button']}>
+      <MicButton isOn={isMicOn} setIsMicOn={setIsMicOn} />
+      <CamButton isOn={isCamOn} setIsCamOn={setIsCamOn} />
+    </div>
+  );
+}
+
+export default StreamButton;
