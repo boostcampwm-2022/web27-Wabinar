d13c9db54254ba384a7159df9f70e55ca7f752e5
dohun <dohun31@naver.com>
2022-11-30 16:19:16 +0900

    feat: 워크스페이스 바뀔 때 momList 갱신

--- client/src/components/Mom/index.tsx
+++ client/src/components/Mom/index.tsx
@@ -1,13 +1,19 @@
-import useMom from 'src/hooks/useSelectedMom';
+import useSelectedMom from 'src/hooks/useSelectedMom';
 
 import Editor from '../Editor';
 import style from './style.module.scss';
 
 function Mom() {
-  const { selectedMom } = useMom();
+  const { selectedMom } = useSelectedMom();
 
   if (!selectedMom) {
-    return <h1>아직 회의록이 없어요. 만들어 보세요^^</h1>;
+    return (
+      <div className={style['mom-container']}>
+        <div className={style['mom']}>
+          <h1>아직 회의록이 없어요. 만들어 보세요^^</h1>
+        </div>
+      </div>
+    );
   }
   // context나 props로 가져오기
 
