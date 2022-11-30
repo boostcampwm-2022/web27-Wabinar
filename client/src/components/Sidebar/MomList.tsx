dca2970f12865d464905d9c306e4504261751e53
se030 <se030.kim@gmail.com>
2022-11-30 09:41:18 +0900

    feat: 회의록 추가 버튼 소켓 서버와 연동

--- client/src/components/Sidebar/MomList.tsx
+++ client/src/components/Sidebar/MomList.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import useSocketContext from 'src/hooks/useSocketContext';
 import { TMom } from 'src/types/mom';
 
 import style from './style.module.scss';
@@ -7,15 +9,37 @@ interface MomListProps {
 }
 
 function MomList({ moms }: MomListProps) {
+  const { momSocket: socket } = useSocketContext();
+
+  const [momList, setMomList] = useState<TMom[]>(moms);
+
+  const onCreateMom = () => {
+    socket.emit('create-mom');
+  };
+
+  useEffect(() => {
+    socket.on('created-mom', (mom) => setMomList((prev) => [...prev, mom]));
+    // TODO: socket.on('selected-mom', () => {});
+
+    return () => {
+      socket.off('created-mom');
+    };
+  }, []);
+
   return (
     <div className={style['mom-list-container']}>
       <h2>회의록</h2>
       <ul className={style['mom-list']}>
-        {moms.map((item) => (
-          <li key={item._id}>{item._id}</li>
+        {momList.map((item) => (
+          <li
+            key={item._id}
+            onClick={() => socket.emit('select-mom', item._id)}
+          >
+            {item._id}
+          </li>
         ))}
       </ul>
-      <button>+ 회의록 추가</button>
+      <button onClick={onCreateMom}>+ 회의록 추가</button>
     </div>
   );
 }
