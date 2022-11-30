b0d8b37f844f70ae57ccb8486f7f0d2819b10e1a
se030 <se030.kim@gmail.com>
2022-11-30 15:21:22 +0900

    refactor: useSelectedMom 네이밍 변경

--- client/src/components/Mom/index.tsx
+++ client/src/components/Mom/index.tsx
@@ -1,20 +1,14 @@
-import useMom from 'src/hooks/useSelectedMom';
+import useSelectedMom from 'src/hooks/useSelectedMom';
 
 import Editor from '../Editor';
 import style from './style.module.scss';
 
 function Mom() {
-  const { selectedMom } = useMom();
+  const { selectedMom } = useSelectedMom();
 
   if (!selectedMom) {
     return <h1>아직 회의록이 없어요. 만들어 보세요^^</h1>;
   }
-  // context나 props로 가져오기
-
-  /*
-    contentEditable 내용을 컨트롤하고 싶다면 ref 활용해볼 수 있음
-    const titleRef = useRef<HTMLHeadingElement>(null);
-  */
 
   const onTitleChange: React.FormEventHandler<HTMLHeadingElement> = (e) => {
     /*
@@ -36,9 +30,9 @@ function Mom() {
             suppressContentEditableWarning={true}
             onInput={onTitleChange}
           >
-            {selectedMom._id}
+            {selectedMom.name}
           </h1>
-          <span>{new Date().toLocaleString()}</span>
+          <span>{new Date(selectedMom.createdAt).toLocaleString()}</span>
         </div>
         <Editor />
       </div>
