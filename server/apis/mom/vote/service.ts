d42dfcf4db8a32476245829c4afdb91225ef5e9d
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-30 14:57:12 +0900

    feat : 투표 생성, 수정 service로직 작성 임시로 Vote 객체로 관리할게요

---
+++ server/apis/mom/vote/service.ts
@@ -0,0 +1,37 @@
+interface Option {
+  id: number;
+  option: string;
+  votedNum: number;
+}
+
+interface Vote {
+  _id: string;
+  title: string;
+  options: Option[];
+}
+
+interface Votes {
+  [id: string]: Vote;
+}
+
+const votes: Votes = {};
+
+export const createVote = (momId: number, vote: Vote) => {
+  votes[momId] = vote;
+  return votes[momId];
+};
+
+export const updateVote = (momId: number, optionId: number) => {
+  const vote = votes[momId];
+
+  vote.options = vote.options.map((option) => {
+    const { id, votedNum } = option;
+
+    if (id === optionId) {
+      return { ...option, votedNum: votedNum + 1 };
+    }
+    return option;
+  });
+
+  return vote.options;
+};
