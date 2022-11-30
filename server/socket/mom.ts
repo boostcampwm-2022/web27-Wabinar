9c01d3e21c8c4d528b9b099016c6522732d364f4
se030 <se030.kim@gmail.com>
2022-11-30 13:55:54 +0900

    feat: momSocketServer에 회의록 crdt 관리 위한 momMap 추가

--- server/socket/mom.ts
+++ server/socket/mom.ts
@@ -1,15 +1,13 @@
 import { createMom, getMom, putMom } from '@apis/mom/service';
 import CRDT from '@wabinar/crdt';
 import { Server } from 'socket.io';
+import LinkedList from '@wabinar/crdt/linked-list';
 
 async function momSocketServer(io: Server) {
-  const momId = '6380c9f7b757041ca21fe96c';
-
-  const { structure } = await getMom(momId);
-
-  const crdt = new CRDT(1, -1, structure);
-
   const workspace = io.of(/^\/sc-workspace\/\d+$/);
+
+  // 회의록 id : 회의록의 crdt 인스턴스
+  const momMap = new Map<string, CRDT>();
 
   workspace.on('connection', async (socket) => {
     const namespace = socket.nsp.name;
@@ -20,6 +18,7 @@ async function momSocketServer(io: Server) {
       return;
     }
 
+    // TODO: 회의 시작과 종료, 소켓 관심사 리팩토링
     socket.on('start-mom', () => {
       workspace.emit('started-mom');
     });
@@ -28,43 +27,77 @@ async function momSocketServer(io: Server) {
       workspace.emit('stoped-mom');
     });
 
-    /* 회의록 선택 시 회의록 정보 불러오기 */
-    socket.on('select-mom', async (roomId) => {
+    /* 회의록 추가하기 */
+    socket.on('create-mom', async () => {
+      const mom = await createMom();
+      const { _id, head, nodeMap } = mom;
+
+      momMap.set(
+        _id.toString(),
+        new CRDT(1, -1, { head, nodeMap } as LinkedList),
+      );
+
+      workspace.emit('created-mom', mom);
+    });
+
+    /* 회의록 선택하기 */
+    socket.on('select-mom', async (momId) => {
+      // 기존 join 되어있던 room은 leave
       const joinedRooms = [
         ...io.of(namespace).adapter.socketRooms(socket.id),
       ].filter((id) => id !== socket.id);
 
       joinedRooms.forEach((room) => socket.leave(room));
 
-      socket.join(roomId);
-      socket.data.roomId = roomId;
+      // 선택된 회의록 room에 join
+      socket.join(momId);
+      socket.data.momId = momId;
 
-      const mom = await getMom(roomId);
+      const mom = await getMom(momId);
+
+      // 서버에 선택된 회의록의 crdt가 없다면 생성
+      if (!momMap.has(momId)) {
+        const { _id, head, nodeMap } = mom;
+
+        momMap.set(
+          _id.toString(),
+          new CRDT(1, -1, { head, nodeMap } as LinkedList),
+        );
+      }
+
+      // 선택된 회의록의 정보 전달
       socket.emit('selected-mom', mom);
     });
 
-    /* 회의록 추가하기 */
-    socket.on('create-mom', async () => {
-      const mom = await createMom();
-      workspace.emit('created-mom', mom);
+    /* crdt initialization */
+    socket.on('mom-initialization', async () => {
+      const crdt = momMap.get(socket.data.momId);
+
+      // TODO: FE에서 initialization 이벤트 플로우 조정
+      socket.emit('mom-initialization', crdt ?? new LinkedList());
     });
 
     /* crdt remote insert delete */
     socket.on('mom-insertion', async (op) => {
-      socket.broadcast.emit('mom-insertion', op);
+      const momId = socket.data.momId;
+      socket.to(momId).emit('mom-insertion', op);
+
+      const crdt = momMap.get(momId);
       crdt.remoteInsert(op);
 
       putMom(momId, crdt.plainData);
     });
 
     socket.on('mom-deletion', async (op) => {
+      const momId = socket.data.momId;
       socket.broadcast.emit('mom-deletion', op);
+
+      const crdt = momMap.get(momId);
       crdt.remoteDelete(op);
 
       putMom(momId, crdt.plainData);
     });
 
-    // 에러 시
     socket.on('error', (err) => {
       console.log(err);
       socket.disconnect();
@@ -72,13 +105,6 @@ async function momSocketServer(io: Server) {
 
     socket.on('disconnect', () => {
       console.log('user disconnected', socket.id);
-    });
-
-    // 초기화에 필요한 정보 전달
-    const { structure } = await getMom(momId);
-
-    socket.on('mom-initialization', () => {
-      socket.emit('mom-initialization', structure);
     });
   });
 }
