02c592b73af25331102eceb481acc64f7c4daa9d
Ruby <happyu9986@gmail.com>
2022-12-01 22:18:49 +0900

    Refactor/#177-S : 소켓 이벤트명 상수화 리팩토링 💢 (#183)
    ## 🤠 개요
    
    - resolve #177 
    - 소켓 이벤트명 상수화 리팩토링
     
    ## 💫 설명
    
    - client랑 server가 공통으로 사용하는 디렉토리가 types, constants 두개가 생길 것 같아서 루트에
    common 아래에 두려고 했는데, `tsconfig.path` 가 말을 안듣네요 ^^..
      - `common/types`는 잘 불러오는데, `common/constants`는 cannot find module 떠요..
    - 이상한게 `socket/mom.ts`에서 못불러오는데 여기서 common으로 가려면 `../../common` 이 경로가
    맞는데, 오류난거 보면 `../../../common`으로 한번 더 들어가더라고요..
      - 세영: 그러니까 못찾지..!!
    
    ![스크린샷 2022-12-01 오후 1 56
    22](https://user-images.githubusercontent.com/63364990/204974460-6ad78408-d4d4-4785-8223-1836ccdfb7d6.png)
    ![스크린샷 2022-12-01 오후 1 56
    32](https://user-images.githubusercontent.com/63364990/204974467-db20184a-d331-4a3f-8930-d4c9303db738.png)
    
    - 아무튼 그래서 그냥 일단은 client, server 각 constants 안에 넣어서 사용하고 있어요ㅠ
    
    ## 🌜 고민거리 (Optional)
    
    ## Q1. server 기준으로 기존 소켓 이벤트명은 on은 start, emit은 started 이런식으로 이름을 다르게
    관리했는데, 하나의 소켓 로직이 하는 일은 하나의 관심사 라고 생각해서 주제를 동사말고 명사로 잡는게 좋지 않을까 생각이 들었어요
    그래서 네이밍은 하나로 통일해서 사용하도록 했어요.
    mom-insertion, mom-creation 이렇게 통일하려고 했는데, start 랑 stop은 뭐로 해야할 지 모르겠어요
    ㅎ
    시그널링 소켓 같은 경우도 offer를 교환하는 과정? 을 표현하고 싶었으나 네이밍이 도저히 생각이 안나서 일단 지금 그대로
    상수화만 해놨어요..
    더 좋은 네이밍 있으면 알려주세요..
    
    ## 📷 스크린샷 (Optional)

--- client/src/components/Sidebar/ConfButton.tsx
+++ client/src/components/Sidebar/ConfButton.tsx
@@ -1,3 +1,4 @@
+import SOCKET_MESSAGE from 'src/constants/socket-message';
 import { useConfContext } from 'src/hooks/useConfContext';
 import useSocketContext from 'src/hooks/useSocketContext';
 import color from 'styles/color.module.scss';
@@ -12,7 +13,7 @@ function ConfButton() {
 
   const onClick = () => {
     setIsStart(!isStart);
-    socket.emit(isStart ? 'stop-mom' : 'start-mom');
+    socket.emit(isStart ? SOCKET_MESSAGE.MOM.END : SOCKET_MESSAGE.MOM.START);
   };
 
   return (
