40301de32afeba516171675e03897fa83ccb13a9
dohun <dohun31@naver.com>
2022-12-05 15:57:47 +0900

    refactor: 빠진 글자 추가
    Resolve: #200

--- client/src/components/Workspace/DefaultWorkspace.tsx
+++ client/src/components/Workspace/DefaultWorkspace.tsx
@@ -9,8 +9,7 @@ function DefaultWorkspace() {
       <SleepingWabIcon />
       <h1 className={style.message}>현재 가입하신 워크스페이스가 없어요.</h1>
       <GuideMessage>
-        <p>왼쪽 + 버튼을 눌러</p>
-        <p>워크스페이스를 만들거나 참여봐요!</p>
+        <p>왼쪽 + 버튼을 눌러 워크스페이스를 만들거나 참여해봐요!</p>
       </GuideMessage>
     </div>
   );
