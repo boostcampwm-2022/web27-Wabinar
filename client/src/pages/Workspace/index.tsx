cf5c15ec519067f744174ad9d80b8e59e2f8b36e
se030 <se030.kim@gmail.com>
2022-12-05 17:07:29 +0900

    refactor: Workspace 컴포넌트에서 유저가 가입한 워크스페이스 정보 요청

--- client/src/pages/Workspace/index.tsx
+++ client/src/pages/Workspace/index.tsx
@@ -1,10 +1,41 @@
 import Workspace from 'components/Workspace';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, Route, Routes } from 'react-router-dom';
+import { getWorkspaces } from 'src/apis/user';
 import DefaultWorkspace from 'src/components/Workspace/DefaultWorkspace';
+import { useUserContext } from 'src/hooks/useUserContext';
 
 import Layout from './Layout';
 
 function WorkspacePage() {
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+
+  const loadWorkspaces = async () => {
+    if (!user) {
+      navigate('/');
+      return;
+    }
+
+    const { id: userId } = user;
+
+    const { workspaces } = await getWorkspaces({ id: userId });
+
+    if (!workspaces.length) {
+      navigate('/workspace');
+      return;
+    }
+
+    const defaultWorkspace = workspaces[0];
+    const { id: workspaceId } = defaultWorkspace;
+
+    navigate(`/workspace/${workspaceId}`);
+  };
+
+  useEffect(() => {
+    loadWorkspaces();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
