ee00bdd6989cb6f12befa3efa55dd2ad0175946d
juyeong-chungbuk <happyu9986@gmail.com>
2022-12-06 19:37:33 +0900

    feat : error interceptor 구현 404, 500만 우선 적용

--- client/src/apis/http.ts
+++ client/src/apis/http.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import env from 'config';
+import { toast } from 'react-toastify';
 
 const SERVER_PATH = env.SERVER_PATH;
 
@@ -7,3 +8,29 @@ export const http = axios.create({
   baseURL: SERVER_PATH,
   withCredentials: true,
 });
+
+http.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response && err.response.status) {
+      switch (err.response.status) {
+        case 401:
+          console.debug(401);
+          break;
+        case 403:
+          console.debug(403);
+          break;
+        case 404:
+          console.debug(404);
+          window.location.href = '/404';
+          break;
+        case 500:
+          console.debug(500);
+          toast('500 Network Error .. ^^', { type: 'error' });
+          break;
+        default:
+          return;
+      }
+    }
+  },
+);
