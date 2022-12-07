bf366fc4426b0c05b7f1952bbb4f4eff588d74f1
dohun <dohun31@naver.com>
2022-12-07 18:04:19 +0900

    feat: 블록 타입 추가

--- client/src/constants/block.ts
+++ client/src/constants/block.ts
@@ -1,18 +1,36 @@
 export const BLOCKS_TYPE = [
   {
+    id: 0,
+    name: '제목 1',
+    desc: '질문할 때 사용해보세요',
+    thumbnail: 'https://www.notion.so/images/blocks/text/ko-KR.png',
+  },
+  {
     id: 1,
+    name: '제목 2',
+    desc: '질문할 때 사용해보세요',
+    thumbnail: 'https://www.notion.so/images/blocks/text/ko-KR.png',
+  },
+  {
+    id: 2,
+    name: '제목 3',
+    desc: '질문할 때 사용해보세요',
+    thumbnail: 'https://www.notion.so/images/blocks/text/ko-KR.png',
+  },
+  {
+    id: 3,
     name: '텍스트',
     desc: '일반 텍스트',
     thumbnail: 'https://www.notion.so/images/blocks/text/ko-KR.png',
   },
   {
-    id: 2,
+    id: 4,
     name: '투표',
     desc: '투표할 때 사용해보세요',
     thumbnail: 'https://www.notion.so/images/blocks/embed.6a481331.png',
   },
   {
-    id: 3,
+    id: 5,
     name: '질문',
     desc: '질문할 때 사용해보세요',
     thumbnail: 'https://www.notion.so/images/blocks/to-do.f8d20542.png',
