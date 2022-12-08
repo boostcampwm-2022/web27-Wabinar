f03b9047893c3d163d370ab3bc6a33fe73b3b99b
dohun <dohun31@naver.com>
2022-12-08 18:25:41 +0900

    feat: MeetingMedia에 username 추가
    Resolve: #290

--- client/src/components/MeetingMediaBar/MeetingMedia/index.tsx
+++ client/src/components/MeetingMediaBar/MeetingMedia/index.tsx
@@ -1,4 +1,5 @@
 import { memo, useEffect, useRef } from 'react';
+import { useUserContext } from 'src/hooks/useUserContext';
 
 import style from './style.module.scss';
 
@@ -9,13 +10,23 @@ interface MediaProps {
 
 function MeetingMedia({ stream, muted }: MediaProps) {
   const ref = useRef<HTMLVideoElement>(null);
+  const { user } = useUserContext();
+
+  const defaultUsername = '익명';
 
   useEffect(() => {
     if (!ref.current) return;
     ref.current.srcObject = stream;
   }, [stream]);
 
-  return <video className={style.video} ref={ref} muted={muted} autoPlay />;
+  return (
+    <>
+      <video className={style.video} ref={ref} muted={muted} autoPlay />
+      <span className={style.username}>
+        {user ? user.name : defaultUsername}
+      </span>
+    </>
+  );
 }
 
 export default memo(MeetingMedia);
