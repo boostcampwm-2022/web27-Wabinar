209e18f1390e2ceb7528b00090cae7f9939edde2
dohun <dohun31@naver.com>
2022-12-08 14:57:50 +0900

    refactor: client 에러 메세지 상수화 및 적용
    Resolve: #270

--- client/src/hooks/useConfContext.ts
+++ client/src/hooks/useConfContext.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
+import ERROR_MESSAGE from 'src/constants/error-message';
 import ConfContext from 'src/contexts/conf';
 
 export function useConfContext() {
   const context = useContext(ConfContext);
 
-  if (!context)
-    throw new Error('ConfContext는 정의된 스코프 안에서만 사용 가능해요 ^^');
+  if (!context) throw new Error(ERROR_MESSAGE.OUT_OF_CONTEXT_SCOPE);
 
   return context;
 }
--- client/src/hooks/useSocketContext.ts
+++ client/src/hooks/useSocketContext.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
+import ERROR_MESSAGE from 'src/constants/error-message';
 import { SocketContext } from 'src/contexts/socket';
 
 export default function useSocketContext() {
   const context = useContext(SocketContext);
 
-  if (!context)
-    throw new Error('SocketContext는 정의된 스코프 안에서만 사용 가능해요 ^^');
+  if (!context) throw new Error(ERROR_MESSAGE.OUT_OF_CONTEXT_SCOPE);
 
   return context;
 }
