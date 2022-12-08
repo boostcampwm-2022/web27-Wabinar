8043c21e5ff21023fcf971f5c8f407b0b1820928
hodun <65100540+dohun31@users.noreply.github.com>
2022-12-08 17:30:41 +0900

    Refactor/#270-B: 에러 메세지 상수화 및 적용 (#273)
    * refactor: server 에러 메세지 상수화 및 적용
    
    Resolve: #270
    
    * refactor: client 에러 메세지 상수화 및 적용
    
    Resolve: #270
    
    * fix: error-message 가 달라서 test 실패 -> CI 실패 에러 해결
    
    * refactor: 오타 수정
    
    * refactor: conflict 해결
    
    * refactor: 오타 수정 적용

--- client/src/hooks/useMeetingContext.ts
+++ client/src/hooks/useMeetingContext.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
+import ERROR_MESSAGE from 'src/constants/error-message';
 import MeetingContext from 'src/contexts/meeting';
 
 export function useMeetingContext() {
   const context = useContext(MeetingContext);
 
-  if (!context)
-    throw new Error('ConfContext는 정의된 스코프 안에서만 사용 가능해요 ^^');
+  if (!context) throw new Error(ERROR_MESSAGE.OUT_OF_CONTEXT_SCOPE);
 
   return context;
 }
--- client/src/hooks/useWorkspacesContext.ts
+++ client/src/hooks/useWorkspacesContext.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
+import ERROR_MESSAGE from 'src/constants/error-message';
 import WorkspacesContext from 'src/contexts/workspaces';
 
 export default function useWorkspacesContext() {
   const context = useContext(WorkspacesContext);
 
-  if (!context)
-    throw new Error('UserContext는 정의된 스코프 안에서만 사용 가능해요 ^^');
+  if (!context) throw new Error(ERROR_MESSAGE.OUT_OF_CONTEXT_SCOPE);
 
   return context;
 }
