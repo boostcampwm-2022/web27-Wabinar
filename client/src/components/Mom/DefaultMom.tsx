ed9aef30d1073d0073969015fa75f97bea06de5e
Seyoung Kim <63814960+se030@users.noreply.github.com>
2022-12-08 20:04:18 +0900

    Style/#271-K: CSS 개선 작업 (#277)
    * style: 워크스페이스 리스트 너비 고정
    
    * refactor: Selector 역할이 드러나도록 네이밍 변경
    
    * style: 워크스페이스 설정 아이콘 기획서대로 스타일링
    
    * style: 사이드바 너비 수정
    
    * fix: Mom에서 임시 투표 블럭 제거
    
    * style: 회의록 스크롤바 추가
    
    * style: 회의록 목록에 임의의 높이 지정하고 스크롤 추가
    
    * style: 사이드바 전체 스크롤 추가
    
    * feat: 회의록 추가 버튼 이동
    
    * style: DefaultMom 컴포넌트 도움말 수정
    
    * style: 회의록 없는 경우 추가 아이콘 하이라이트
    
    * style: 로그인 페이지 스타일 수정

--- client/src/components/Mom/DefaultMom.tsx
+++ client/src/components/Mom/DefaultMom.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { RiFileAddLine } from '@react-icons/all-files/ri/RiFileAddLine';
 import GuideIcon from 'src/components/common/GuideIcon';
 import AmazedWabIcon from 'src/components/common/Icon/Wab/AmazedWabIcon';
 
@@ -11,8 +11,13 @@ function DefaultMom() {
       <h1 className={style.message}>회의록이 텅텅 비었어요.</h1>
       <GuideIcon>
         <p>
-          사이드바에서 <span className={style.highlight}>+ 회의록 추가</span>{' '}
-          버튼을 클릭해 회의록을 추가해 보세요!
+          사이드바에서{' '}
+          <span className={style.highlight}>
+            <RiFileAddLine size={20} />
+          </span>{' '}
+          버튼을 클릭해
+          <br />
+          회의록을 추가해 보세요!
         </p>
       </GuideIcon>
     </div>
