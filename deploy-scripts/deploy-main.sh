c52ea763da8b9802ac8f17f2a87d7e81d6bcbf11
se030 <se030.kim@gmail.com>
2022-12-08 10:48:19 +0900

    refactor: 배포 스크립트 파일로 분리

---
+++ deploy-scripts/deploy-main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd deploy-scripts
+
+source ./config.sh # 프로젝트 디렉토리 환경변수 설정
+
+cd ${PROJECT_BASE_DIR}
+git pull origin main
+
+npm ci
+npm run deploy # main 브랜치 빌드
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd deploy-scripts
-(source try-dotenv-vault-login.sh) # 배포 환경 .env 설정
-
-source ./config.sh # 프로젝트 디렉토리 환경변수 설정
-
-cd ${PROJECT_BASE_DIR}
-git pull origin main
-
-npm ci
-npm run deploy
+(source try-dotenv-vault-login.sh) # dotenv-vault 사용을 위한 로그인
+(source deploy-main.sh) # 배포 스크립트
