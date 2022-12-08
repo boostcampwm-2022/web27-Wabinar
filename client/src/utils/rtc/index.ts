f00c59322bdf52f677870e0857c0092e582a7b2a
W. Cho <wcho.dev@gmail.com>
2022-12-08 23:18:58 +0900

    fix: 두번째 연결부터 ice에 오류 메시지가 생기는 이슈 해결
    리팩토링 이전 코드로 되돌림

--- client/src/utils/rtc/index.ts
+++ client/src/utils/rtc/index.ts
@@ -1,8 +1,6 @@
 import { WORKSPACE_EVENT } from '@wabinar/constants/socket-message';
 import { Socket } from 'socket.io-client';
 
-type SocketId = string;
-
 type onMediaConnectedCb = (socketId: string, remoteStream: MediaStream) => void;
 type onMediaDisconnectedCb = (socketId: string) => void;
 
@@ -30,25 +28,6 @@ class RTC {
     this.onMediaDisconnectedCallback = () => {}; // eslint-disable-line @typescript-eslint/no-empty-function
   }
 
-  connect() {
-    this.socket.on(
-      WORKSPACE_EVENT.RECEIVE_HELLO,
-      this.setDescriptionAndEmitOffer.bind(this),
-    );
-    this.socket.on(
-      WORKSPACE_EVENT.RECEIVE_OFFER,
-      this.setDescriptionAndEmitAnswer.bind(this),
-    );
-    this.socket.on(
-      WORKSPACE_EVENT.RECEIVE_ANSWER,
-      this.setDescriptionForAnswer.bind(this),
-    );
-    this.socket.on(WORKSPACE_EVENT.RECEIVE_ICE, this.addIce.bind(this));
-    this.socket.on(WORKSPACE_EVENT.RECEIVE_BYE, this.disconnectPeer.bind(this));
-
-    this.socket.emit(WORKSPACE_EVENT.SEND_HELLO);
-  }
-
   onMediaConnected(callback: onMediaConnectedCb) {
     this.onMediaConnectedCallback = callback;
   }
@@ -57,99 +36,41 @@ class RTC {
     this.onMediaDisconnectedCallback = callback;
   }
 
-  private async setDescriptionAndEmitOffer(remoteSocketId: SocketId) {
-    const pc = this.createPeerConnection(remoteSocketId);
-    this.connections.set(remoteSocketId, pc);
-
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
-
-    this.socket.emit(WORKSPACE_EVENT.SEND_OFFER, offer, remoteSocketId);
-  }
-
-  private async setDescriptionAndEmitAnswer(
-    offer: RTCSessionDescriptionInit,
-    remoteSocketId: SocketId,
-  ) {
-    const pc = this.createPeerConnection(remoteSocketId);
-    await pc.setRemoteDescription(offer);
-    this.connections.set(remoteSocketId, pc);
-
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-
-    this.socket.emit(WORKSPACE_EVENT.SEND_ANSWER, answer, remoteSocketId);
-  }
-
-  private async setDescriptionForAnswer(
-    answer: RTCSessionDescriptionInit,
-    remoteSocketId: SocketId,
-  ) {
-    const pc = this.connections.get(remoteSocketId);
-    if (!pc) {
-      throw new Error('No RTCPeerConnection on answer received.');
-    }
-
-    await pc.setRemoteDescription(answer);
-  }
-
-  private async addIce(ice: RTCIceCandidate, remoteSocketId: SocketId) {
-    const pc = this.connections.get(remoteSocketId);
-    if (!pc) {
-      throw new Error('No RTCPeerConnection on ice candindate received.');
-    }
-
-    await pc.addIceCandidate(ice);
-  }
-
-  private async disconnectPeer(remoteSocketId: SocketId) {
-    this.connections.delete(remoteSocketId);
-    this.streams.delete(remoteSocketId);
-
-    this.onMediaDisconnectedCallback(remoteSocketId);
-  }
-
-  private createPeerConnection(remoteSocketId: string) {
+  #createPeerConnection(remoteSocketId: string) {
     // initialize
     const pcOptions = {
       iceServers: [{ urls: this.iceServerUrls }],
     };
     const pc = new RTCPeerConnection(pcOptions);
 
-    pc.addEventListener('icecandidate', (event) =>
-      this.emitIce(event, remoteSocketId),
-    );
-    pc.addEventListener('track', (event) =>
-      this.addRemoteStream(event, remoteSocketId),
-    );
-    this.addTracksToLocalConnection(pc);
+    // add event listeners
+    pc.addEventListener('icecandidate', (iceEvent) => {
+      this.socket.emit(
+        WORKSPACE_EVENT.SEND_ICE,
+        iceEvent.candidate,
+        remoteSocketId,
+      );
+    });
+    pc.addEventListener('track', async (event) => {
+      if (this.streams.has(remoteSocketId)) {
+        return;
+      }
+
+      const [remoteStream] = event.streams;
+
+      this.streams.set(remoteSocketId, remoteStream);
+      this.onMediaConnectedCallback(remoteSocketId, remoteStream);
+    });
+
+    // add tracks
+    this.userMediaStream
+      .getTracks()
+      .forEach((track) => pc.addTrack(track, this.userMediaStream));
 
     return pc;
   }
 
-  private emitIce(event: RTCPeerConnectionIceEvent, remoteSocketId: string) {
-    this.socket.emit(WORKSPACE_EVENT.SEND_ICE, event.candidate, remoteSocketId);
-  }
-
-  private addRemoteStream(event: RTCTrackEvent, remoteSocketId: string) {
-    if (this.streams.has(remoteSocketId)) {
-      return;
-    }
-
-    const [remoteStream] = event.streams;
-
-    this.streams.set(remoteSocketId, remoteStream);
-    this.onMediaConnectedCallback(remoteSocketId, remoteStream);
-  }
-
-  private addTracksToLocalConnection(pc: RTCPeerConnection) {
-    this.userMediaStream
-      .getTracks()
-      .forEach((track) => pc.addTrack(track, this.userMediaStream));
-  }
-
-  // unused; may be used in the future
-  private async setVideoBitrate(pc: RTCPeerConnection, bitrate: number) {
+  async #setVideoBitrate(pc: RTCPeerConnection, bitrate: number) {
     // fetch video sender
     const [videoSender] = pc
       .getSenders()
@@ -159,6 +80,68 @@ class RTC {
     const params = videoSender.getParameters();
     params.encodings[0].maxBitrate = bitrate;
     await videoSender.setParameters(params);
+  }
+
+  connect() {
+    this.socket.on(WORKSPACE_EVENT.RECEIVE_HELLO, async (remoteSocketId) => {
+      const pc = this.#createPeerConnection(remoteSocketId);
+      this.connections.set(remoteSocketId, pc);
+
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+
+      this.socket.emit(
+        WORKSPACE_EVENT.SEND_OFFER,
+        pc.localDescription,
+        remoteSocketId,
+      );
+    });
+
+    this.socket.on(
+      WORKSPACE_EVENT.RECEIVE_OFFER,
+      async (offer, remoteSocketId) => {
+        const pc = this.#createPeerConnection(remoteSocketId);
+        this.connections.set(remoteSocketId, pc);
+
+        await pc.setRemoteDescription(offer);
+
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+
+        this.socket.emit(WORKSPACE_EVENT.SEND_ANSWER, answer, remoteSocketId);
+      },
+    );
+
+    this.socket.on(
+      WORKSPACE_EVENT.RECEIVE_ANSWER,
+      async (answer, remoteSocketId) => {
+        const pc = this.connections.get(remoteSocketId);
+        if (!pc) {
+          throw new Error('No RTCPeerConnection on answer received.');
+        }
+
+        await pc.setRemoteDescription(answer);
+      },
+    );
+
+    this.socket.on(WORKSPACE_EVENT.RECEIVE_ICE, (ice, remoteSocketId) => {
+      const pc = this.connections.get(remoteSocketId);
+
+      if (!pc) {
+        throw new Error('No RTCPeerConnection on ice candindate received.');
+      }
+
+      pc.addIceCandidate(ice);
+    });
+
+    this.socket.on(WORKSPACE_EVENT.RECEIVE_BYE, (remoteSocketId) => {
+      this.connections.delete(remoteSocketId);
+      this.streams.delete(remoteSocketId);
+
+      this.onMediaDisconnectedCallback(remoteSocketId);
+    });
+
+    this.socket.emit(WORKSPACE_EVENT.SEND_HELLO);
   }
 }
 
