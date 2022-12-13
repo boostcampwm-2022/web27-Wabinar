a33904025f0546c834db12886a6bd3b6018a5cd2
juyeong-chungbuk <happyu9986@gmail.com>
2022-12-13 15:10:46 +0900

    feat : 기존 미디어 스트림은 readonly 이므로 setState props 제거

--- client/src/components/MeetingMediaBar/StreamButton/index.tsx
+++ client/src/components/MeetingMediaBar/StreamButton/index.tsx
@@ -1,26 +1,19 @@
-import { Dispatch, memo, SetStateAction } from 'react';
+import { memo } from 'react';
 
 import CamButton from './CamButton';
 import MicButton from './MicButton';
 import style from './style.module.scss';
 
 interface StreamButtonProps {
-  isMicOn: boolean;
-  isCamOn: boolean;
-  setIsMicOn?: Dispatch<SetStateAction<boolean>>;
-  setIsCamOn?: Dispatch<SetStateAction<boolean>>;
+  isMicOn?: boolean;
+  isCamOn?: boolean;
 }
 
-function StreamButton({
-  isMicOn,
-  isCamOn,
-  setIsMicOn,
-  setIsCamOn,
-}: StreamButtonProps) {
+function StreamButton({ isMicOn, isCamOn }: StreamButtonProps) {
   return (
     <div className={style['stream-button']}>
-      <MicButton isOn={isMicOn} setIsMicOn={setIsMicOn} />
-      <CamButton isOn={isCamOn} setIsCamOn={setIsCamOn} />
+      <MicButton isOn={isMicOn} />
+      <CamButton isOn={isCamOn} />
     </div>
   );
 }
