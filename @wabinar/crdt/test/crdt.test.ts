b58c047dcad819fe4527449efceb38b01e1078e0
se030 <se030.kim@gmail.com>
2022-12-13 21:45:53 +0900

    fix: localDelete 테스트 메서드 수정 및 에러 throw

--- @wabinar/crdt/test/crdt.test.ts
+++ @wabinar/crdt/test/crdt.test.ts
@@ -7,31 +7,31 @@ describe('CRDT Local operations', () => {
   beforeEach(() => {
     const initialStructure = new LinkedList();
     나 = new CRDT(1, initialStructure);
-  })
+  });
 
   describe('localInsert()', () => {
     it('head 위치 삽입에 성공한다.', () => {
       // act
       나.localInsert(-1, '녕');
       나.localInsert(-1, '안');
-  
+
       // assert
       expect(나.read()).toEqual('안녕');
     });
-  
+
     it('tail 위치 삽입에 성공한다.', () => {
       // act
       나.localInsert(-1, '안');
       나.localInsert(0, '녕');
-  
+
       // assert
       expect(나.read()).toEqual('안녕');
     });
-  
+
     it('없는 위치에 삽입 시도 시 에러를 던진다.', () => {
       // arrange
       나.localInsert(-1, '안');
-      
+
       // act & assert
       expect(() => 나.localInsert(1, '녕')).toThrow();
     });
@@ -45,17 +45,17 @@ describe('CRDT Local operations', () => {
 
       // act
       나.localDelete(1);
-  
+
       // assert
       expect(나.read()).toEqual('안');
     });
-  
+
     it('없는 위치에 삭제 시도 시 에러를 던진다.', () => {
       // arrange
       나.localInsert(-1, '안');
-  
+
       // act & assert
-      expect(() => 나.localInsert(1, '녕')).toThrow();
+      expect(() => 나.localDelete(1)).toThrow();
     });
   });
 });
