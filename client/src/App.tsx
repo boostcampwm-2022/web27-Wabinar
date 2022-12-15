08a812b1f70bdfedf667d393161d207faef4843b
Ruby <happyu9986@gmail.com>
2022-12-15 22:50:23 +0900

    Feat/#380-S : React.lazy + Suspense 적용 (#391)
    * feat : suspense fallback에 적용시킬 원크스페이스 skeleton ui 구현
    
    * feat : route 별 React.lazy 및 Suspense 적용
    
    * chore : 자동 import sorting
    
    * chore : page index 파일 삭제
    
    * perf : 로고, 버블 아이콘 jpg로 변경
    
    * refactor : 스켈레톤 UI에  기존 레이아웃만 가져오도록 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { getAuth } from 'src/apis/auth';
 import UserContext from 'src/contexts/user';
-import {
-  LoadingPage,
-  LoginPage,
-  NotFoundPage,
-  OAuthPage,
-  WorkspacePage,
-} from 'src/pages';
 import { User } from 'src/types/user';
 
 import 'styles/reset.scss';
 import MetaHelmet from './components/MetaHelmet';
 
+const LoginPage = lazy(() => import('src/pages/Login'));
+const OAuthPage = lazy(() => import('src/pages/OAuth'));
+const WorkspacePage = lazy(() => import('src/pages/Workspace'));
+const NotFoundPage = lazy(() => import('src/pages/404'));
+const LoadingPage = lazy(() => import('src/pages/Loading'));
+
 function App() {
   const [user, setUser] = useState<User | null>(null);
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -24,11 +22,9 @@ function App() {
   const autoLogin = async () => {
     const { user } = await getAuth();
 
-    setIsLoaded(true);
-
     setUser(user);
 
-    if (user && !/^\/workspace(\/\d)?$/.test(location.pathname)) {
+    if (user && !/^\/workspace(\/\d+)?$/.test(location.pathname)) {
       navigate('/workspace');
     }
   };
@@ -40,7 +36,7 @@ function App() {
   return (
     <>
       <MetaHelmet title="화상회의와 회의록 작성을 한번에, Wabinar" />
-      {isLoaded ? (
+      <Suspense fallback={<LoadingPage />}>
         <UserContext.Provider value={{ user, setUser }}>
           <Routes>
             <Route path="/" element={<LoginPage />} />
@@ -49,9 +45,7 @@ function App() {
             <Route path="/404" element={<NotFoundPage />} />
           </Routes>
         </UserContext.Provider>
-      ) : (
-        <LoadingPage />
-      )}
+      </Suspense>
     </>
   );
 }
---
+++ client/src/components/WorkspaceList/Skeleton.tsx
@@ -0,0 +1,7 @@
+import style from './style.module.scss';
+
+function WorkspaceListSkeleton() {
+  return <div className={style['workspace-list-container']}></div>;
+}
+
+export default WorkspaceListSkeleton;
