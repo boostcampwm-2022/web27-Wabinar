14bc9d7a353aee900402bc63b281b7814c157224
se030 <se030.kim@gmail.com>
2022-12-15 22:57:54 +0900

    fix: socket/Mom socket/mom 으로 변경

---
+++ server/socket/mom/handleQuestionBlock.ts
@@ -0,0 +1,37 @@
+import { addQuestion } from '@apis/mom/block/question/service';
+import { getBlock, putBlock } from '@apis/mom/block/service';
+import { BlockType } from '@wabinar/constants/block';
+import { BLOCK_EVENT } from '@wabinar/constants/socket-message';
+import { Server, Socket } from 'socket.io';
+
+export default function handleQuestionBlock(
+  io: Server,
+  namespace: string,
+  socket: Socket,
+) {
+  socket.on(BLOCK_EVENT.FETCH_QUESTIONS, async (blockId) => {
+    const { questionProperties: questions } = await getBlock(blockId);
+
+    socket.emit(`${BLOCK_EVENT.FETCH_QUESTIONS}-${blockId}`, questions);
+  });
+
+  socket.on(BLOCK_EVENT.ADD_QUESTIONS, async (blockId, question) => {
+    const momId = socket.data.momId;
+
+    await addQuestion(blockId, question);
+
+    io.of(namespace)
+      .to(momId)
+      .emit(`${BLOCK_EVENT.ADD_QUESTIONS}-${blockId}`, question);
+  });
+
+  socket.on(BLOCK_EVENT.RESOLVE_QUESTIONS, async (blockId, questions) => {
+    const momId = socket.data.momId;
+
+    await putBlock(blockId, BlockType.QUESTION, questions);
+
+    io.of(namespace)
+      .to(momId)
+      .emit(`${BLOCK_EVENT.FETCH_QUESTIONS}-${blockId}`, questions);
+  });
+}
---
+++ server/socket/mom/handleVoteBlock.ts
@@ -0,0 +1,43 @@
+import {
+  createVote,
+  endVote,
+  Option,
+  getVoteResult,
+  updateVote,
+} from '@apis/mom/block/vote/service';
+import { BLOCK_EVENT } from '@wabinar/constants/socket-message';
+import { Server, Socket } from 'socket.io';
+
+export default function handleVoteBlock(
+  io: Server,
+  namespace: string,
+  socket: Socket,
+) {
+  socket.on(BLOCK_EVENT.REGISTER_VOTE, async (blockId, options: Option[]) => {
+    const momId = socket.data.momId;
+
+    await createVote(blockId, options);
+
+    socket.to(momId).emit(`${BLOCK_EVENT.REGISTER_VOTE}-${blockId}`, options);
+  });
+
+  socket.on(BLOCK_EVENT.UPDATE_VOTE, async (blockId, optionId, userId) => {
+    const momId = socket.data.momId;
+
+    const participantCount = await updateVote(blockId, optionId, userId);
+
+    io.of(namespace)
+      .to(momId)
+      .emit(`${BLOCK_EVENT.UPDATE_VOTE}-${blockId}`, participantCount);
+  });
+
+  socket.on(BLOCK_EVENT.END_VOTE, async (blockId) => {
+    const momId = socket.data.momId;
+
+    const block = await endVote(blockId);
+
+    const res = await getVoteResult(block);
+
+    io.of(namespace).to(momId).emit(`${BLOCK_EVENT.END_VOTE}-${blockId}`, res);
+  });
+}
