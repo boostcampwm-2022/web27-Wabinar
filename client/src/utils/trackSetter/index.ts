aa15ac66e27c13921bd68f274e603314c9727061
Won-hee Cho <89635107+wcho21@users.noreply.github.com>
2022-12-15 18:31:04 +0900

    Test/#381-C: trackSetter 모듈 테스트 (#382)
    * test: trackSetter 모듈
    * chore: test 스크립트 추가

--- client/src/utils/trackSetter/index.ts
+++ client/src/utils/trackSetter/index.ts
@@ -1,33 +1,27 @@
 export type TrackKind = 'audio' | 'video';
 
 const getAllTracks = async (stream: MediaStream) => {
-  if (!stream) {
-    return;
-  }
-  
   const tracks = stream.getTracks();
+
   return tracks;
-}
+};
 
-const getTrack = async (stream: MediaStream, kind: TrackKind) => {
+export const getTrack = async (stream: MediaStream, kind: TrackKind) => {
   const tracks = await getAllTracks(stream);
-  if (!tracks) {
-    return;
-  }
+  const track = tracks.find((track) => track.kind === kind);
 
-  const track = tracks.find(track => track.kind === kind);
   return track;
-}
+};
 
-export const setTrack = async (stream: MediaStream, kind: TrackKind, turnOn: boolean) => {
-  if (!stream) {
-    return;
-  }
-
+export const setTrack = async (
+  stream: MediaStream,
+  kind: TrackKind,
+  turnOn: boolean,
+) => {
   const track = await getTrack(stream, kind);
   if (!track) {
     return;
   }
 
   track.enabled = turnOn;
-}
+};
