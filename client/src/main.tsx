6dc17bf6d46ef6ec9d7c93b6bf542f7c444945f1
se030 <se030.kim@gmail.com>
2023-05-31 12:34:56 +0900

    chore: recoil 설치

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
+import { RecoilRoot } from 'recoil';
 import 'react-toastify/dist/ReactToastify.css';
+
 import App from './App';
 import Toaster from './components/common/Toaster';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>,
+  <RecoilRoot>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </RecoilRoot>,
 );
